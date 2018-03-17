25753a042d12701db0659e99971fe84ac4240a5c
Roger Moore <rmoore74@live.co.uk>
2018-03-17 16:36:10 +0000

    removed useless else

--- 1/multiples_of_3_and_5.py
+++ 1/multiples_of_3_and_5.py
@@ -1,11 +1,7 @@
 total = 0
 
 for i in range(1000):
-    if (i % 3 == 0) & (i % 5 == 0):
-        total += i
-    elif i % 3 == 0:
-        total += i
-    elif i % 5 == 0:
+   if (i % 3 == 0) | (i % 5 == 0):
         total += i
 
 print(total)

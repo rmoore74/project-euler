6e4925c195d25b9ba7db579cde2cbd4a76e32846
Roger Moore <rmoore74@live.co.uk>
2018-03-17 16:22:41 +0000

    completed first task

---
+++ 1/multiples_of_3_and_5.py
@@ -0,0 +1,11 @@
+total = 0
+
+for i in range(1000):
+    if (i % 3 == 0) & (i % 5 == 0):
+        total += i
+    elif i % 3 == 0:
+        total += i
+    elif i % 5 == 0:
+        total += i
+
+print(total)
